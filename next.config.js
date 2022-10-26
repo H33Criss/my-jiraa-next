c65627527bd7927fd391a00c65a482a4a85b32cc
Cristobal-coding <agartkg95@gmail.com>
2022-10-25 22:12:21 -0300

    add new cors

--- next.config.js
+++ next.config.js
@@ -2,10 +2,33 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/api/(.*)",
+  //       headers: [
+  //         { key: "Access-Control-Allow-Credentials", value: "true" },
+  //         { key: "Access-Control-Allow-Origin", value: "https://myjiraa-nextjs.herokuapp.com" },
+
+  //       ]
+  //     }
+  //   ]
+  // },
+  // headers: [
+  //   { key: "Access-Control-Allow-Credentials", value: "true" },
+  //   { key: "Access-Control-Allow-Origin", value: "https://myjiraa-nextjs.herokuapp.com" },
+  //   // ...
+  // ]
+}
+
+// module.exports = nextConfig
+module.exports = {
   async headers() {
     return [
       {
-        source: "/api/(.*)",
+        source: "/_next/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "https://myjiraa-nextjs.herokuapp.com" },
@@ -14,11 +37,4 @@ const nextConfig = {
       }
     ]
   },
-  // headers: [
-  //   { key: "Access-Control-Allow-Credentials", value: "true" },
-  //   { key: "Access-Control-Allow-Origin", value: "https://myjiraa-nextjs.herokuapp.com" },
-  //   // ...
-  // ]
 }
-
-module.exports = nextConfig
