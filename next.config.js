32292035776f04b4be73b6f1962209977ebc8358
Cristobal-coding <agartkg95@gmail.com>
2022-10-25 21:55:18 -0300

    adding headers

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  headers: [
+    { key: "Access-Control-Allow-Credentials", value: "true" },
+    { key: "Access-Control-Allow-Origin", value: "https://myjiraa-nextjs.herokuapp.com" },
+    // ...
+  ]
 }
 
 module.exports = nextConfig
